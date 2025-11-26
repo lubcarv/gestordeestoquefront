2e5b545307d5f05f6db88ca78472c7fcb7e08d84
lubcarv <luiza.bc26@aluno.ifsc.edu.br>
2025-11-26 17:38:21 -0300

    Update api.js

--- gestordeestoque_front/api.js
+++ gestordeestoque_front/api.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = "https://gestordeestoque.onrender.com";
+const API_BASE_URL = "https://gestordeestoque.onrender.com/swagger-ui/index.html";
 
 async function apiRequest(path, options = {}) {
   const url = `${API_BASE_URL}${path}`;
@@ -291,3 +291,4 @@ const DashboardAPI = {
     return apiRequest(`/api/dashboard/ultimas-movimentacoes?limite=${limite}`);
   }
 };
+
